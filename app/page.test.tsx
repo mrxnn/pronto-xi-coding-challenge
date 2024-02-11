532eaad0b064494c67ec5be48df395ffe56e076a
Mayura <mayura.ramanayake@gmail.com>
2024-02-11 11:57:19 +0530

    More testing to ensure that robot doesnt move beyond edges

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -30,16 +30,34 @@ describe("Home Page", () => {
 
   test("robot does not move beyond table boundaries", async () => {
     const { getByTestId } = render(<Page />);
+    const robot = getByTestId("robot");
 
+    // should not move beyond left and bottom edges
     fireEvent.click(getByTestId("left-control"));
     fireEvent.click(getByTestId("bottom-control"));
     await delay();
-
-    const robot = getByTestId("robot");
     expect(robot?.style.bottom).toBe("0px");
     expect(robot?.style.left).toBe("0px");
+
+    // should not move beyond top and right edges
+    fireEvent.click(getByTestId("top-control"));
+    fireEvent.click(getByTestId("top-control"));
+    fireEvent.click(getByTestId("top-control"));
+    fireEvent.click(getByTestId("top-control"));
+    fireEvent.click(getByTestId("top-control"));
+    fireEvent.click(getByTestId("right-control"));
+    fireEvent.click(getByTestId("right-control"));
+    fireEvent.click(getByTestId("right-control"));
+    fireEvent.click(getByTestId("right-control"));
+    fireEvent.click(getByTestId("right-control"));
+    await delay();
+    expect(robot?.style.bottom).toBe("400px");
+    expect(robot?.style.left).toBe("400px");
   });
 });
 
+// utility to delay for the next iteration of the nodejs event loop
+// plus, framer-motion animations runs outside the react's lifecycle
+// even if the animations are disabled with MotionConfig.
 const delay = (ms: number = 200) =>
   new Promise((resolve) => setTimeout(() => resolve(1), ms));
